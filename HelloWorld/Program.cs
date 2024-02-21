bf8df7d3d77d83269bbf6a4bc8cdf95b7cdab4d6
Yasyasmine11 <yasmineaou40@gmail.com>
2024-02-21 22:56:18 +0100

    fin

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ // use CNTRL F5 to exec
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
