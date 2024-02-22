2128241b3d395b357b7d87c074a8ae66d343ceb4
Yasyasmine11 <yasmineaou40@gmail.com>
2024-02-23 00:34:56 +0100

    update_chapter_four

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,14 +5,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
- // use CNTRL F5 to exec
+// use CNTRL F5 to exec
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // First step : printing our first sentence
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("*************");
+
+            // Second : Using Variables
+            byte number = 2;
+            int count = 255;
+            float note = 15.23f;
+            char letter = 'r';
+            string name = "Dangeon";
+            bool isValable = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(note);
+            Console.WriteLine(letter);
+            Console.WriteLine(name);
+            Console.WriteLine(isValable);
+            Console.WriteLine("*************");
+
+            // Third : Formating
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("*************");
+
+            // Fourth : Conversion 
+            byte i = 2;
+            int j = (int)i;
+
+            string a = "5";
+            int b = Convert.ToInt32(a);
+            // Or
+            int c = int.Parse(a);
+
+            try
+            {
+                var num = "12525";
+                byte bi = Convert.ToByte(num);
+                Console.WriteLine(bi);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Can't convert this number to a byte !!");
+            }
+            Console.WriteLine("*************");
+
+            // Fifth : Operators
+
+
         }
     }
 }
